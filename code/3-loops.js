3004775dd0bba2f6192ce1ce56bd41ccd0869ccb
t-kupp <kupper.thorge@gmail.com>
2024-10-27 15:37:37 +0100

    q3 done

--- code/3-loops.js
+++ code/3-loops.js
@@ -1,8 +1,16 @@
 // Loops allow you to execute code repeatedly based on a condition.
 
 // Task 1: Write a for loop to print numbers from 1 to 5.
+for (let i = 1; i < 6; i++) {
+  console.log(i);
+}
 
 // Task 2: Create a while loop that prints the numbers 10 to 15.
+let n = 10;
+while (n < 16) {
+  console.log(n);
+  n++;
+}
 
 // Task 3. Write for-loop that loops through the array `numbers`
 // compare every number with the variable `lowest`.
@@ -11,5 +19,22 @@
 const numbers = [9, 10, 17, 34, 2, 4, 100];
 let lowest = numbers[0];
 
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] < lowest) {
+    lowest = numbers[i];
+  }
+}
+console.log(lowest);
+
 // Task 4, Write a function `getLowest` that takes an array of numbers as
 // input and returns the lowest number.
+function getLowest(arr) {
+  let lowest = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < lowest) {
+      lowest = arr[i];
+    }
+  }
+  return lowest;
+}
+console.log(getLowest([19, 11, 7, 100, 4, 44]));
